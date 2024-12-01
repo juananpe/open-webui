d85172c67c76cf2bdadf046e7bca54357730c97e
Timothy Jaeryang Baek <tim@openwebui.com>
2024-11-30 22:29:53 -0800

    refac

--- backend/open_webui/apps/retrieval/main.py
+++ backend/open_webui/apps/retrieval/main.py
@@ -882,7 +882,7 @@ def save_docs_to_vector_db(
         return True
     except Exception as e:
         log.exception(e)
-        return False
+        raise e
 
 
 class ProcessFileForm(BaseModel):
@@ -912,7 +912,7 @@ def process_file(
 
             docs = [
                 Document(
-                    page_content=form_data.content,
+                    page_content=form_data.content.replace("<br/>", "\n"),
                     metadata={
                         **file.meta,
                         "name": file.filename,
